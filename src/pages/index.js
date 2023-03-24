296e91946e388c78cd54e31fd7c6ebe5486c2433
= <jargalan.dev@gmail.com>
2023-03-24 15:13:40 +0800

    zas

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,10 @@
 import React from 'react';
+import { Hello } from 'mbank-component';
 
 export default function Home() {
-    return <div>HOME</div>;
+    return (
+        <div>
+            <Hello />
+        </div>
+    );
 }
